5a334ab31add57f964c1b1dd6a965260e5df63ea
Thanh Nam <honam867@gmail.com>
2023-12-20 23:40:13 +0700

    not init mongo

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ const { initDB } = require("./src/init/db");
 
 (async function init() {
   const express = initExpress();
-  await initDB(express);
+  // await initDB(express);
   initRouter(express);
 })();
